2740b887a5af1eb1aace7fbcbda3a62afccda34b
GaoFan <gaofan104@yahoo.com>
2015-09-17 00:02:00 -0400

    committed on 17/09/2015

--- app/PersonalProfile.php
+++ app/PersonalProfile.php
@@ -4,6 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class PersonalProfile
+ * @package App
+ */
 class PersonalProfile extends Model
 {
     protected $table = 'personalProfiles';
